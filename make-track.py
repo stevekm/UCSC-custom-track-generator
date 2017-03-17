658f797d0b2e3d733164c0150b5814371c001cdd
Stephen Kelly <stevekm@users.noreply.github.com>
2017-03-17 19:01:34 -0400

    add base files

---
+++ make-track.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+'''
+This script will make custom UCSC tracks
+resources:
+https://genome.ucsc.edu/
+https://genome.ucsc.edu/goldenpath/help/bigBed.html
+https://genome.ucsc.edu/goldenpath/help/hgTracksHelp.html
+https://genome.ucsc.edu/goldenpath/help/customTrack.html
+
+https://genome.ucsc.edu/cgi-bin/hgCustom
+'''
+
+
+# ~~~~ LOAD PACKAGES ~~~~~~ #
+import sys
+import os
+import argparse
+
+# ~~~~ CUSTOM FUNCTIONS ~~~~~~ #
+def my_debugger(vars):
+    '''
+    starts interactive Python terminal at location in script
+    very handy for debugging
+    call this function with
+    my_debugger(globals().copy())
+    anywhere in the body of the script, or
+    my_debugger(locals().copy())
+    within a script function
+    '''
+    import readline # optional, will allow Up/Down/History in the console
+    import code
+    # vars = globals().copy() # in python "global" variables are actually module-level
+    vars.update(locals())
+    shell = code.InteractiveConsole(vars)
+    shell.interact()
+
+def get_data_type(input_file):
+    import os
+    file_name, extension = os.path.splitext(input_file)
+    # dictionary of types recognized
+    # file extension : (type, URL parameter)
+    data_types = {".bw": ("bigWig","bigDataUrl"), ".bed": ("BED", "url")}
+    if extension in data_types.keys():
+        return(data_types[extension])
+    else:
+        print("File type not recognized for file:\n{}\nExiting...".format(input_file))
+        sys.exit()
+
+def get_URL_param(file_type):
+    # list of file extensions for 'bigData' filetypes
+    bigData_ext = ['.bw', '.bam', '.cram', '.bb', '.vcf'] #  BAM, CRAM, bigBed, bigWig or VCF
+
+
+# ~~~~ GET SCRIPT ARGS ~~~~~~ #
+parser = argparse.ArgumentParser(description='UCSC Custom Track Creator.')
+# required positional args
+parser.add_argument("input_files", nargs='+', help="path to the file(s) to create tracks from") # , nargs='?'
+
+# required flags
+parser.add_argument("-url", default = 'http://somewhere.com/somedir/', type = str, dest = 'url', metavar = 'URL', help="Externally accessible URL base to use for the files")
+
+# optional flags
+parser.add_argument("-g", default = 'hg19', type = str, dest = 'genome', metavar = 'genome', help="Name of the reference genome, Defaults to hg19")
+
+
+args = parser.parse_args()
+input_files = args.input_files
+
+
+if __name__ == "__main__":
+    print(input_files)
+    for file in input_files:
+        print(get_data_type(file))
